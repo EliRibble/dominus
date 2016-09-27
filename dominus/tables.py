c0781355cd25c15c6c3387121442938ac28c01ef
Eli Ribble <eli@authentise.com>
2016-09-27 14:33:38 -0600

    Add basic upgrades for alembic to get going and card.type

--- dominus/tables.py
+++ dominus/tables.py
@@ -7,8 +7,9 @@ Set = chryso.schema.table('set',
     sqlalchemy.Column('name', sqlalchemy.String(256), nullable=False),
 )
 
-Cards = chryso.schema.table('card',
+Card = chryso.schema.table('card',
     sqlalchemy.Column('name', sqlalchemy.String(256), nullable=False),
-    sqlalchemy.Column('text', sqlalchemy.String(1024), nullable=False),
     sqlalchemy.Column('set', None, sqlalchemy.ForeignKey('set.uuid'), nullable=False),
+    sqlalchemy.Column('text', sqlalchemy.String(1024), nullable=False),
+    sqlalchemy.Column('type', sqlalchemy.String(1024), nullable=False),
 )
