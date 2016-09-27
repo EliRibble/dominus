e9b5930e7b1865ff1835680c14de58fc9a88d043
Eli Ribble <eli@authentise.com>
2016-09-27 14:35:32 -0600

    Break out DB setup separately, enable debug

--- dominus/main.py
+++ dominus/main.py
@@ -6,23 +6,27 @@ import flask
 import dominus.tables
 import dominus.views
 
+def setup_db():
+    db = "postgres://dev:development@localhost:5432/dominus"
+    engine = chryso.connection.Engine(db, dominus.tables)
+    chryso.connection.store(engine)
 
 def run():
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
+
+    setup_db()
 
     app = flask.Flask('dominus')
     app.config.update(dict(
         FLASK_DEBUG = True,
         SECRET_KEY = 'development key',
     ))
-    db = "postgres://dev:development@localhost:5432/dominus"
-    engine = chryso.connection.Engine(db, dominus.tables)
-    chryso.connection.store(engine)
 
     app.route('/')(dominus.views.root)
+    app.route('/admin/')(dominus.views.admin)
 
     try:
-        app.run('localhost', '4554')
+        app.run(host='localhost', port=4554, debug=True)
     except KeyboardInterrupt:
         print("Shutting down...")
